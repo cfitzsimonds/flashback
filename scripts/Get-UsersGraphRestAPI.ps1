b97be822e9e23abbec80c86d2ea67c6510306102
Chris Fitzsimonds <CFitzsimonds@rightpoint.com>
2020-08-17 10:34:15 -0400

    updating

---
+++ scripts/Get-UsersGraphRestAPI.ps1
@@ -0,0 +1,7 @@
+﻿# Connect with the Microsoft Graph via PnP PowerShell
+# https://docs.microsoft.com/en-us/powershell/sharepoint/sharepoint-pnp/sharepoint-pnp-cmdlets?view=sharepoint-ps
+Connect-PnPOnline -Scopes "User.ReadWrite.All"
+$accesstoken = Get-PnPAccessToken
+ 
+$response = Invoke-RestMethod -Uri https://graph.microsoft.com/v1.0/users/ -ContentType "application/json" -Headers @{Authorization = "Bearer $accesstoken"} -Method Get
+$response
---
+++ scripts/Update-UserPhoto.ps1
@@ -0,0 +1,18 @@
+﻿$UserCredential = Get-Credential 
+$Session = New-PSSession -ConfigurationName Microsoft.Exchange -ConnectionUri https://outlook.office365.com/powershell-liveid/?proxyMethod=RPS -Credential $UserCredential -Authentication Basic -AllowRedirection 
+Import-PSSession $Session -AllowClobber
+ 
+#Update the user photos path here. Name of the file should be username of the Office365 user. 
+$path= 'D:\O365Images\' 
+$Images = Get-ChildItem $path 
+$Images | Foreach-Object { 
+    $SplitUsername = ($_.Name.Tostring() -split "\.")
+    $Identity = $SplitUsername[0]+"."+$SplitUsername[1]+"."+$SplitUsername[2]
+    $Identity
+    $PictureData = $path+$_.name 
+    $PictureData
+    Write-Host "Setting user photo..."
+    Set-UserPhoto -Identity $Identity -PictureData ([System.IO.File]::ReadAllBytes($PictureData)) -Confirm:$false
+    Write-Host "Done"
+}
+
